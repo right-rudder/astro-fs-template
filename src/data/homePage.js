5fd54d87e85cc7bd0d92bee81608b0ed2cef8bfa
Tim Jedrek <tim@timjedrek.com>
2024-06-17 17:25:04 -0500

    build out data file

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -1,9 +1,53 @@
 const homePage = {
   data: {
     //SEO Header Stuff
-    pageTitle: "helllo",
+    pageTitle: "XYZ Flight School in Rudderville, RRM",
     pageDescription: "meta description goes here",
-    page2: "asdasd",
+    pageKeywords: "add keywords here.  take a look at the consts.ts file for more guidance on keywords",
+
+    //top header
+    headerImagePath: "",
+    headerH1: "",
+    headerH2: "",
+
+    //contentOne
+    contentOneImage: "",
+    contentOneImageAlt: "",
+    contentOneImageCaption: "",
+    contentOneH3: "",
+    contentOneParagraphs: ["", ""],
+    contentOneButtonText: "",
+    contentOneButtonLink: "",
+
+    //contentTwo
+    contentTwoImage: "",
+    contentTwoImageAlt: "",
+    contentTwoImageCaption: "",
+    contentTwoH3: "",
+    contentTwoParagraphs: ["", ""],
+    contentTwoButtonText: "",
+    contentTwoButtonLink: "",
+
+    //contentThree
+    contentThreeImage: "",
+    contentThreeImageAlt: "",
+    contentThreeImageCaption: "",
+    contentThreeH3: "",
+    contentThreeParagraphs: ["", ""],
+    contentThreeButtonText: "",
+    contentThreeButtonLink: "",
+
+    //Bottom CTAOne
+    CTAOneH2: "",
+    CTAOneH3: "",
+    CTAOneButtonText: "",
+    CTAOneButtonLink: "",
+
+    //Bottom CTATwo
+    CTATwoH2: "",
+    CTATwoH3: "",
+    CTATwoButtonText: "",
+    CTATwoButtonLink: "",
   }
 }
 
