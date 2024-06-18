8208a9005906bb0cb9ff00fefcd21d66d1d2da09
Tim Jedrek <tim@timjedrek.com>
2024-06-17 21:50:24 -0500

    home

--- src/data/homePage.js
+++ src/data/homePage.js
@@ -6,49 +6,58 @@ const homePage = {
     pageKeywords: "add keywords here.  take a look at the consts.ts file for more guidance on keywords",
 
     //top header
-    headerImagePath: "../assets/flight-school-name-location-keyword.jpg",
+    headerImagePath: "/src/assets/flight-school-name-location-keyword.jpg",
     headerImageAlt: "Plane flying overhead XYZ location at ABC Flight School",
     headerH1: "123 Flight School",
     headerH2: "Rudderville Anytown's best flight school",
 
     //contentOne
-    contentOneImage: "",
-    contentOneImageAlt: "",
-    contentOneImageCaption: "",
-    contentOneH3: "",
-    contentOneParagraphs: ["", ""],
-    contentOneButtonText: "",
-    contentOneButtonLink: "",
+    contentOneImage: "/src/assets/flightschool-name-pilots-in-airliner-cockpit.jpg",
+    contentOneImageAlt: "Flight School XYZ Alumni now flying jets in the airlines",
+    contentOneImageCaption: "Flight School XYZ Alumni now flying jets in the airlines",
+    contentOneH3: "Do you think you could be a pilot",
+    contentOneParagraphs: [
+      "Are you fascinated by the world of aviation? Have you always dreamed of soaring through the skies? Do you find yourself looking up at the planes overhead, wondering what it would be like to be the one in control.", 
+      "Start by taking our quick quiz to see if you have what it takes! Our quiz will assess your knowledge and skills related to aviation, as well as your personality traits and interests, to help determine if flying is right for you."
+    ],
+    contentOneButtonText: "TAKE QUIZ",
+    contentOneButtonLink: "/pilot-quiz",
 
     //contentTwo
-    contentTwoImage: "",
-    contentTwoImageAlt: "",
-    contentTwoImageCaption: "",
-    contentTwoH3: "",
-    contentTwoParagraphs: ["", ""],
-    contentTwoButtonText: "",
-    contentTwoButtonLink: "",
+    contentTwoImage: "/src/assets/flightschool-name-pilots-in-airliner-cockpit.jpg", 
+    contentTwoImageAlt: "Flight School XYZ Alumni now flying jets in the airlines",
+    contentTwoImageCaption: "Flight School XYZ Alumni now flying jets in the airlines",
+    contentTwoH3: "Why XYZ Flight School",
+    contentTwoParagraphs: [
+      "Becoming a pilot is no easy task.  But with the right guidance and support, <strong>everyone has the opportunity</strong> to learn how to become a pilot.  We proudly offer you the best service and personalized instruction at XYZ flight school.  Serving the ABC Land area for the past 30 years, we've been helping ABC people become pilots with our modern Piper aircraft fleet.", 
+      "Become the next RightRuddersville airline pilot!  We have flight training programs for all different experience levels.  From just starting out and doing an introductory discovery flight (your first flight lesson), to earning your Airline Transport Pilot (ATP) certificates; 123 Flight School is not just your flight school, it's your second family and community of aviators."
+    ],
+    contentTwoButtonText: "VIEW PROGRAMS",
+    contentTwoButtonLink: "/pilot-training-programs",
 
     //contentThree
-    contentThreeImage: "",
-    contentThreeImageAlt: "",
-    contentThreeImageCaption: "",
-    contentThreeH3: "",
-    contentThreeParagraphs: ["", ""],
-    contentThreeButtonText: "",
-    contentThreeButtonLink: "",
+    contentThreeImage: "/src/assets/flightschool-name-pilots-in-airliner-cockpit.jpg", 
+    contentThreeImageAlt: "Flight School XYZ Alumni now flying jets in the airlines",
+    contentThreeImageCaption: "Flight School XYZ Alumni now flying jets in the airlines",
+    contentThreeH3: "How to Become a Pilot",
+    contentThreeParagraphs: [
+      "Start today and take the first step in becoming a pilot at XYZ flight school.  Becoming a pilot and enroll at ABC Flight School.  First, get in contact with us by calling us or filling out a form on our website.  A ABC Flight School team member will contact you.", 
+      "We have programs for all experience levels: Private Pilot, instrument rating, commercial certificate, airline transport pilot"
+    ],
+    contentThreeButtonText: "CONTACT US",
+    contentThreeButtonLink: "/contact",
 
     //Bottom CTAOne
-    CTAOneH2: "",
-    CTAOneH3: "",
-    CTAOneButtonText: "",
-    CTAOneButtonLink: "",
+    CTAOneH2: "The professional flight school to be a pilot.",
+    CTAOneH3: "Best in class fleet, awesome instructors that care about your success. Rudderville residents know where to go to become a pilot.",
+    CTAOneButtonText: "SCHEDULE A VIST",
+    CTAOneButtonLink: "/schedule",
 
     //Bottom CTATwo
-    CTATwoH2: "",
-    CTATwoH3: "",
-    CTATwoButtonText: "",
-    CTATwoButtonLink: "",
+    CTATwoH2: "Visit qwerty pilot academy",
+    CTATwoH3: "TAKE THE NEXT STEP",
+    CTATwoButtonText: "contact us",
+    CTATwoButtonLink: "/contact",
   }
 }
 
